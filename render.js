be55fcee5b62531ff972e05a0c6494ba878f4fa3
Alexandria201 <aov6lt@ukr.net>
2022-01-26 14:56:20 +0200

    Configuration

--- render.js
+++ render.js
@@ -1,36 +1,34 @@
 function userName() {
   const name = document.getElementById('name').value;
   if (name.length === 0) {
-    return "Guest";
-  } else {
-    return name;
+    return 'Guest';
   }
+  return name;
 }
 
 function salary() {
-  const salary = +document.getElementById('salary').value;
-  if (salary <= 0) {
+  const money = +document.getElementById('salary').value;
+  if (money <= 0) {
     return 1;
-  } else {
-    return salary;
   }
+  return money;
 }
 
 function period() {
-  const period = document.getElementById('period').value;
-  if (period <= 0) {
+  const time = document.getElementById('period').value;
+  if (time <= 0) {
     return 1;
-  } else {
-    return period;
   }
+  return time;
 }
 
 function countSalary() {
   return salary() * period();
 }
 
+// eslint-disable-next-line no-unused-vars
 function displayText() {
   const text = `Привет ${userName()}, твой доход ${salary()}  грн. за ${period()} месяца будет 
   ${countSalary()} грн.`;
-document.getElementById('result').innerHTML = text;
+  document.getElementById('result').innerHTML = text;
 }
